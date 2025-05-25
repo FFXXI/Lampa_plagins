0c7dc6c2f130c427bdff994a894cccd5ec2059bc
FFXXI <74616180+FFXXI@users.noreply.github.com>
2025-05-26 00:29:49 +0400

    Update anitop.js

--- anitop.js
+++ anitop.js
@@ -1,61 +1,111 @@
-// Main Menu Exit
-(function () {
-    "use strict";
+(function(){
+  "use strict";
 
-    Lampa.Lang.add({
-        exit_menu: {
-            ru: "Выход",
-            en: "Exit",
-            uk: "Вихід",
-            be: "Вынахад",
-            zh: "出口",
-            pt: "Saída",
-            bg: "Изход"
+  // Добавляем языковую поддержку для пункта меню
+  Lampa.Lang.add({
+    anime_parser: {
+      ru: "Аниме парсер",
+      en: "Anime Parser",
+      uk: "Anime Parser",
+      be: "Anime Parser"
+    }
+  });
+
+  // Конструктор плагина парсера аниме
+  function AnimeParser(){
+    this.name = "anime_parser";
+    this.type = "plugin";
+    this.version = "1.0";
+    this.author = "ВашеИмя"; // замените на свое имя или ник
+  }
+
+  // Инициализация плагина
+  AnimeParser.prototype.init = function(){
+    console.log("AnimeParser: Инициализация плагина");
+    this.addMenuItem();
+  };
+
+  // Функция добавления пункта меню в интерфейс Lampa
+  AnimeParser.prototype.addMenuItem = function(){
+    if (window.Lampa && Lampa.Menu && typeof Lampa.Menu.append === "function"){
+      // Пример простой иконки (SVG) с буквой A
+      var ico = '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">' +
+                  '<circle cx="32" cy="32" r="32" fill="#4CAF50"/>' +
+                  '<text x="32" y="42" font-size="24" text-anchor="middle" fill="#fff">A</text>' +
+                '</svg>';
+      var menu_item = $(
+        '<li class="menu__item selector" data-action="anime_parser">' +
+          '<div class="menu__ico">' + ico + '</div>' +
+          '<div class="menu__text">' + Lampa.Lang.translate("anime_parser") + '</div>' +
+        '</li>'
+      );
+      // При клике на пункт меню вызывается обработчик onClick
+      menu_item.on("hover:enter", this.onClick.bind(this));
+      // Добавляем элемент в меню – во второй список меню, как в известных плагинах
+      $(".menu .menu__list").eq(1).append(menu_item);
+      console.log("AnimeParser: Пункт меню добавлен");
+    } else {
+      console.error("AnimeParser: Lampa.Menu.append не найден");
+    }
+  };
+
+  // Обработчик клика по пункту меню
+  AnimeParser.prototype.onClick = function(){
+    console.log("AnimeParser: Пункт меню нажат");
+    // Запускаем парсинг аниме (в этом примере используется тестовый список)
+    this.loadAnime();
+  };
+
+  // Функция, выполняющая «парсинг» или загрузку списка аниме
+  AnimeParser.prototype.loadAnime = function(){
+    console.log("AnimeParser: Начало загрузки аниме");
+    // Здесь можно сделать Ajax-запрос к вашему API или распарсить HTML с внешнего ресурса.
+    // Для примера создадим тестовый список аниме.
+    var animeList = [
+      { title: "Naruto", image: "https://via.placeholder.com/300", url: "https://example.com/naruto" },
+      { title: "One Piece", image: "https://via.placeholder.com/300", url: "https://example.com/onepiece" },
+      { title: "Attack on Titan", image: "https://via.placeholder.com/300", url: "https://example.com/aot" }
+    ];
+    // После загрузки данных открываем экран с аниме
+    this.showAnimeScreen(animeList);
+  };
+
+  // Функция отображения экрана с полученным списком аниме
+  AnimeParser.prototype.showAnimeScreen = function(animeList){
+    console.log("AnimeParser: Открытие экрана аниме");
+    if (window.Lampa && Lampa.Controller && typeof Lampa.Controller.add === "function" && typeof Lampa.Controller.toggle === "function"){
+      var html = '<div class="anime-parser-list" style="display:flex; flex-wrap:wrap; justify-content:center;">';
+      animeList.forEach(function(anime){
+        html += '<div class="anime-item" style="margin:10px; border:1px solid #fff; padding:10px; max-width:300px;">' +
+                  '<img src="'+anime.image+'" alt="'+anime.title+'" style="width:100%;">' +
+                  '<div style="text-align:center; margin-top:5px;">'+ anime.title +'</div>' +
+                '</div>';
+      });
+      html += '</div>';
+      
+      var view = {
+        name: "anime_parser_view",
+        component: "view",
+        template: function(){
+          return '<div style="padding:20px; color:white;">' + html + '</div>';
         }
-    });
+      };
+      Lampa.Controller.add(view);
+      Lampa.Controller.toggle("anime_parser_view");
+      console.log("AnimeParser: Экран с аниме открыт");
+    } else {
+      console.error("AnimeParser: Lampa.Controller не доступен");
+    }
+  };
 
-    function exit_m(object) {
-        this.create = function () { };
-        this.build = function () { }; // this.activity.loader(false);
-        this.start = function () { };
-        this.pause = function () { };
-        this.stop = function () { };
-        this.render = function () { };
-        this.destroy = function () { };
-    }
-    function add() {
-        var ico =
-            '<svg version="1.1" id="exit" color="#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\n	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">\n<g>\n	<path fill="currentColor" d="M256,5.1c138.6,0,250.9,112.3,250.9,250.9S394.6,506.9,256,506.9S5.1,394.6,5.1,256S117.4,5.1,256,5.1z\n		 M256,40.1C136.7,40.1,40.1,136.7,40.1,256S136.7,471.9,256,471.9S471.9,375.3,471.9,256S375.3,40.1,256,40.1z M311.4,176.6\n		c6.7-6.7,17.5-6.7,24.2,0c6.7,6.7,6.7,17.5,0,24.2l-55.1,55.1l55.1,55c6.7,6.7,6.7,17.5,0,24.2c-6.7,6.7-17.5,6.7-24.2,0L256.3,280\n		l-55.1,55.1c-6,6-15.4,6.6-22.1,1.8l-2.2-1.8c-6.7-6.7-6.7-17.5,0-24.2l55.1-55l-55.1-55c-6.7-6.7-6.7-17.5,0-24.2s17.5-6.7,24.2,0\n		l55.1,55.1L311.4,176.6z"/>\n</g>\n</svg>';
-        var menu_items = $(
-            '<li class="menu__item selector" data-action="exit_r"><div class="menu__ico">' +
-            ico +
-            '</div><div class="menu__text">' +
-            Lampa.Lang.translate("exit_menu") +
-            "</div></li>"
-        );
-        menu_items.on("hover:enter", function () {
-            Lampa.Activity.out();
-            if (Lampa.Platform.is('apple_tv'))
-                window.location.assign('exit://exit');
-            if (Lampa.Platform.is("tizen"))
-                tizen.application.getCurrentApplication().exit();
-            if (Lampa.Platform.is("webos")) window.close();
-            if (Lampa.Platform.is("android")) Lampa.Android.exit();
-            if (Lampa.Platform.is("orsay")) Lampa.Orsay.exit();
-            if (Lampa.Platform.is("nw")) nw.Window.get().close();
-        });
-        $(".menu .menu__list").eq(1).append(menu_items);
-    }
-
-    function createExitMenu() {
-        window.plugin_exit_m_ready = true;
-        Lampa.Component.add("exit_m", exit_m);
-        if (window.appready) add();
-        else {
-            Lampa.Listener.follow("app", function (e) {
-                if (e.type == "ready") add();
-            });
-        }
-    }
-    if (!window.plugin_exit_m_ready) createExitMenu();
+  // Регистрируем плагин через систему плагинов Lampa
+  if(window.Lampa && Lampa.Plugin && typeof Lampa.Plugin.register === "function"){
+    var plugin = new AnimeParser();
+    Lampa.Plugin.register({ name: plugin.name, type: plugin.type, version: plugin.version, author: plugin.author }, plugin);
+    plugin.init();
+    console.log("AnimeParser: Плагин успешно зарегистрирован");
+  } else {
+    console.error("AnimeParser: Lampa.Plugin.register не найден");
+  }
+  
 })();
